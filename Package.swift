0ee6b36a3ba737bf72f50171a6649f294a63087a
Paweł Madej <pawel.madej@amukana.pl>
2020-09-11 21:21:47 +0200

    Rename SPM target

--- Package.swift
+++ Package.swift
@@ -8,8 +8,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "twitter-text",
-            targets: ["twitter-text"]),
+            name: "TwitterText",
+            targets: ["TwitterText"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -20,10 +20,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "twitter-text",
+            name: "TwitterText",
             dependencies: []),
         .testTarget(
-            name: "twitter-textTests",
-            dependencies: ["twitter-text"]),
+            name: "TwitterTextTests",
+            dependencies: ["TwitterText"]),
     ]
 )
